3492d8934bb85f22173b8221bc838035aa56dbd6
Nathanael Weber <nathanael.weber@bbcag.ch>
2017-07-05 20:08:08 +0200

    Initial commit

---
+++ src/main/kotlin/ch/weber/gfocusfx/view/MainView.kt
@@ -0,0 +1,12 @@
+package ch.weber.gfocusfx.view
+
+import ch.weber.gfocusfx.app.Styles
+import tornadofx.*
+
+class MainView : View("Hello TornadoFX") {
+    override val root = hbox {
+        label(title) {
+            addClass(Styles.heading)
+        }
+    }
+}
